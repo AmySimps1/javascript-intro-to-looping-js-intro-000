812bfb681eec1ff2ac2eb2553d8203a457213cd2
Amy Simpson <simpson.r.amy@gmail.com>
2020-11-17 20:50:59 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,9 +8,9 @@ for ([initialization]; [condition]; [iteration]) {
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
   if (i === 1) {
-    array.push('I am 1 strange loop.')
+    array.push('I am 1 strange loop.');
   } else {
-    array.push(`I am ${i} strange loops.`)
+    array.push(`I am ${i} strange loops.`);
   }
   }
   return array;
@@ -26,7 +26,7 @@ while ([condition]) {
 function whileLoop(n) {
   while (n > 0) {
   console.log(n);
- n -= 1;
+ n --;
   }
   return 'done';
 }
